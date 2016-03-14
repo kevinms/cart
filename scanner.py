75f09e472e70868d4f56f93d035d4c4450c48d99
Kevin M. Smith <ksmith.nop@gmail.com>
2016-03-14 01:34:16 -0400

    Barcode lookup.

--- scanner.py
+++ scanner.py
@@ -1,4 +1,17 @@
+import httplib
+
+#012800517725
 
 while True:
 	barcode = raw_input("Enter barcode: ")
-	print "Read barcode: " + barcode
+	barcode.rstrip()
+	url = 'www.upcdatabase.com'
+	print url
+
+	c = httplib.HTTPConnection(url)
+	c.request("GET", "/item/" + barcode)
+	r = c.getresponse()
+	print r.status, r.reason
+	data = r.read()
+	print data
+
